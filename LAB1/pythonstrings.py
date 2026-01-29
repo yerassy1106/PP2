4041d22d0f5f8fc008597a33b7e90b8037a10274
Yerassyl <ibragimoverasyl546@gmail.com>
2026-01-29 18:36:24 +0500

    Add Practice1 Python basics examples

---
+++ LAB1/pythonstrings.py
@@ -0,0 +1,15 @@
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+a = """big city life omir ozen agady
+      atten i wanna be child again
+      saby bolgym keledy"""
+print(a)
+b = "Hello, World!"
+print(b[2:5])
+print(b[:5])
+print(b.upper())
+print(b.lower())
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
