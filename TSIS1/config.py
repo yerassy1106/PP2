b0462e8f280aad58e57ff9ae88a770ea549806f8
Yerassyl <ibragimoverasyl546@gmail.com>
2026-04-29 08:38:47 +0500

    TSIS1

---
+++ TSIS1/config.py
@@ -0,0 +1,7 @@
+# db connection settings
+DB_CONFIG = {
+    "host": "localhost",
+    "database": "phonebook_db",
+    "user": "postgres",
+    "password": "Erasyl2006"
+}
---
+++ TSIS1/connect.py
@@ -0,0 +1,8 @@
+import psycopg2
+from config import DB_CONFIG
+
+
+def get_connection():
+    """just connect to the database using the settings from config"""
+    print(DB_CONFIG)
+    return psycopg2.connect(**DB_CONFIG)
